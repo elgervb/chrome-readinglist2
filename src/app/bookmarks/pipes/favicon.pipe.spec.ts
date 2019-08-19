cbc5ab0248e6a142fb86bfea1c403562152cd132
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2019-08-19 16:57:04 +0200

    #10 move from angular builder to plain jest. Better integration with VSCode

--- src/app/bookmarks/pipes/favicon.pipe.spec.ts
+++ src/app/bookmarks/pipes/favicon.pipe.spec.ts
@@ -1,8 +1,44 @@
 import { FaviconPipe } from './favicon.pipe';
 
 describe('FaviconPipe', () => {
+  let pipe: FaviconPipe;
+
+  beforeEach(() => pipe = new FaviconPipe());
+
   it('create an instance', () => {
-    const pipe = new FaviconPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return null when not passing a bookmark', () => {
+    expect(pipe.transform(null)).toBe(null);
+  });
+
+  it('should return null when not passing a bookmark with an url', () => {
+    const bookmark: chrome.bookmarks.BookmarkTreeNode = {
+      title: 'should return null when not passing a bookmark with an url',
+      id: '1',
+      url: undefined
+    };
+    expect(pipe.transform(bookmark)).toBe(null);
+  });
+
+  it('should return the favicon url', () => {
+    const url = 'http://google.com';
+    const bookmark: chrome.bookmarks.BookmarkTreeNode = {
+      title: 'should return null when not passing a bookmark with an url',
+      id: '1',
+      url
+    };
+    expect(pipe.transform(bookmark)).toBe(`${url}/favicon.ico`);
+  });
+
+  it('should return the favicon url with portnumber', () => {
+    const url = 'http://google.com:8080';
+    const bookmark: chrome.bookmarks.BookmarkTreeNode = {
+      title: 'should return null when not passing a bookmark with an url',
+      id: '1',
+      url
+    };
+    expect(pipe.transform(bookmark)).toBe(`${url}/favicon.ico`);
   });
 });
--- src/app/bookmarks/services/version/version.service.spec.ts
+++ src/app/bookmarks/services/version/version.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { VersionService } from './version.service';
+jest.mock('src/manifest.json', () => ({ version: '99.99.99.unittest' }));
 
 describe('VersionService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +9,10 @@ describe('VersionService', () => {
   it('should be created', () => {
     const service: VersionService = TestBed.get(VersionService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return the version', () => {
+    const service: VersionService = TestBed.get(VersionService);
+    expect(service.getVersion()).toBe('99.99.99.unittest');
   });
 });
