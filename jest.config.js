ea2a18bc2a4ba4e452cd7858e58a7a74bf65e12d
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2019-08-21 08:09:37 +0200

    #10 add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -23,10 +23,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 35,
-      branches: 10,
-      functions: 19,
-      lines: 30
+      statements: 50,
+      branches: 35,
+      functions: 50,
+      lines: 50
     }
   },
   moduleNameMapper: {
