96d0a961fc0971d4ea45d859abc1be603a4c7c7c
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-01-11 22:51:20 +0100

    #49 fix linting errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { GoogleAnalyticsService } from '@core/google-analytics.service';
@@ -8,7 +8,7 @@ describe('AppComponent', () => {
     sendPageView: jest.fn()
   };
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
