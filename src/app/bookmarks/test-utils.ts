89849dd3b80fe1140633fdf068a1f87600549990
Elger <elgervb@gmail.com>
2025-02-11 14:21:41 +0100

    #121 migrate eslint

--- src/app/bookmarks/test-utils.ts
+++ src/app/bookmarks/test-utils.ts
@@ -2,13 +2,13 @@ export function createBookmark(args?: { url: string }): chrome.bookmarks.Bookmar
   return {
     id: guid(),
     title: guid(),
-    url: args && args.url ? args.url : `http://${guid()}`
+    url: args?.url ? args.url : `http://${guid()}`
   };
 }
 
 export function guid(): string {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-    // eslint-disable-next-line no-bitwise
+
     const r = Math.random() * 16 | 0; const v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
